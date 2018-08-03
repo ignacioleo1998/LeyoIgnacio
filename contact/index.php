e3dbed7ef801e287b0b787ea8e6712c96d655127
ignacioleo1998 <leoagcaoili.ignacio0806@gmail.com>
2018-08-03 16:01:43 +0800

    Initial

---
+++ contact/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8"/>
+    
+    <title>Leo - Work</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="keywords" content="leo, ignacio, leo ignacio, web design, websites, student, developer" />
+
+    <meta name="description" content="The portfolio of Leo Ignacio, Student Web Designer / Web Develoer based in Philippines" />
+
+    <link rel="icon" href="../pictures/leo-logo.png"> 
+
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+
+    <link rel="stylesheet" type="text/css" href="../css/style-smartphones.css">
+
+    <link rel="stylesheet" type="text/css" href="../css/style-ipad.css">
+
+    <link rel="stylesheet" type="text/css" href="../css/style-large-screen.css">
+
+    <link rel="stylesheet" type="text/css" href="../css/style-desktop-laptop.css">
+
+</head>
+
+<body style="background-image: url('');">
+
+<!-- navbar -->
+  <section class="navigation">
+    <div class="nav-container">
+      <div class="brand">
+        <a href="#!">L e o I g n a c i o</a>
+          </div>
+            <nav>
+              <div class="nav-mobile">
+                <a id="nav-toggle" href="#!"><span></span></a>
+                  </div>
+                  <ul class="nav-list">
+                <li><a href="../index.php">About</a></li>
+              <li><a href="index.php">Work</a></li>
+            <li><a href="../skill/index.php">My Skill</a></li>
+          <li><a class="active" href="#!">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </section>
+<!--/.navbar-->
+
+
+<div id="stage" class="stage"></div>
+
+<!-- The Content
+<div class="content">
+
+<footer>
+    <p class="footer-text">© 2018 Leo Ignacio. All rights reserved.</p>
+</footer>
+
+</div>
+
+/The Content -->
+
+
+</body>
+
+  <script type="text/javascript" src="../js/jquery.min.js"></script>
+  <script type="text/javascript" src="../js/typed.min.js"></script>
+  <script type="text/javascript" src="../js/style.js"></script>
+
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8"/>
+
+	<title>Leo Ignacio</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta name="keywords" content="leo, ignacio, leo ignacio, web design, websites, student, developer" />
+
+	<meta name="description" content="The portfolio of Leo Ignacio, Student Web Designer / Web Develoer based in Philippines" />
+
+	<link rel="icon" href="pictures/leo-logo.png"> 
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+  	<link rel="stylesheet" type="text/css" href="css/style-smartphones.css">
+
+  	<link rel="stylesheet" type="text/css" href="css/style-ipad.css">
+
+  	<link rel="stylesheet" type="text/css" href="css/style-large-screen.css">
+
+  	<link rel="stylesheet" type="text/css" href="css/style-desktop-laptop.css">
+
+</head>
+<body>
+
+<div class="container">
+  <div class="one-half left">
+    <h1>Leo Ignacio</h1>
+    <a href="#" class="cta">Let's Go</a>
+  </div>
+  <div class="one-half right">
+    <h1>Designer/Developer</h1>
+    <a href="#" class="cta">Let's Go</a>
+  </div>
+</div>
+
+
+</body>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/style.js"></script>
+</html>
